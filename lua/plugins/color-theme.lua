b8e56b6d12986c6f0cc30def704e656ec6d0ea06
jchan <juan.carlos.chan@gmail.com>
2024-01-08 01:37:41 -0600

    nocticshc colorscheme added

--- lua/plugins/color-theme.lua
+++ lua/plugins/color-theme.lua
@@ -1,13 +1,14 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		name = "kanagawa",
-		priority = 1000,
-		config = function()
-			require("kanagawa").load("wave")
-		end,
-	},
-	{
-		-- "nyoom-engineering/oxocarbon.nvim",
-	},
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 1000,
+    config = function()
+      require("kanagawa").load("wave")
+    end,
+  },
+  {
+    -- "nyoom-engineering/oxocarbon.nvim",
+  },
+  { "iagorrr/noctishc.nvim", name = "noctishc", priority = 1000 },
 }
